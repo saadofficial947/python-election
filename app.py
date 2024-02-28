ce3785f1cb1fccb292062d84cf4b34d22644b4e7
saadofficial947 <161262043+saadofficial947@users.noreply.github.com>
2024-02-28 08:20:39 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def contact_us():
 
 def main():
     st.title("BANO QABIL - Online Election System")
-    st.image("C:\\Users\\Muhammad Sajjad\\Downloads\\pexels-element-digital-1550337.jpg", width=200)
+    st.image('r'"C:\\Users\\Muhammad Sajjad\\Downloads\\pexels-element-digital-1550337.jpg", width=200)
     
     election = Election()  # Move election instance outside main() to persist data between function calls
 
